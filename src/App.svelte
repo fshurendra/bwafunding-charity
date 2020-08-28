2679cbf36e559b1b26a8170d1240e3b195fe27f3
fshurendra <fshurendra@gmail.com>
2020-08-28 23:38:51 +0700

    Add header

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,18 @@
-<H1>Fauzi Shurendra</H1>
+<script>
+	import CharityList from "./CharityList.svelte"
+	
+	let title = "Fauzi";
+	setTimeout(function() {
+		title ="Shurendra";
+	}, 2000);
+</script>
+
+<h1>{title}</h1>
+<CharityList/>
+
+<style>
+	h1 {
+		color: aqua;
+		font-size: 3em;
+	}
+</style>
